7b2a7111975b0f4c1577b5bdaac541e6649d5c2c
Martin Hans <martinhans@gmail.com>
2023-02-24 10:12:22 +0100

    Lower minimal temperatures to ease printing in winter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -610,7 +610,13 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   5
+//
+// Heater 0 (hotend) uses thermistor type 1, which is defined down to a
+// temperature of -15 C. Set minimum temperature to -5.
+//
+// Bed uses thermistor type 5, which is defined down to a temperature of
+// 0C. Set minimum temperature to 1.
+#define HEATER_0_MINTEMP   -5
 #define HEATER_1_MINTEMP   5
 #define HEATER_2_MINTEMP   5
 #define HEATER_3_MINTEMP   5
@@ -618,7 +624,7 @@
 #define HEATER_5_MINTEMP   5
 #define HEATER_6_MINTEMP   5
 #define HEATER_7_MINTEMP   5
-#define BED_MINTEMP        5
+#define BED_MINTEMP        1
 #define CHAMBER_MINTEMP    5
 
 // Above this temperature the heater will be switched off.
